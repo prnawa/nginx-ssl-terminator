fb937f2c7004ca117552d4b25ffcd6e25f9cd504
Ruwan Nawarathne <prnawa@gmail.com>
2014-05-28 18:05:57 +0800

    Fixed missing comma

--- attributes/sslCerts.rb
+++ attributes/sslCerts.rb
@@ -9,14 +9,14 @@ default["nginx"]["sslcertkey"]["mysitesslcert"] = {
 	:cert => [
 		"-----BEGIN CERTIFICATE-----",
 		"MIIDKjCCApOgAwIBAgIJAP1tLjj7nrbsMA0GCSqGSIb3DQEBBQUAMIGtMQswCQYD",
-		".........."
+		"..........",
 		"yp3yAas+YT2BV/DNsdJZmOMFQh3OLKkh9D8idR6kS+HAtJK/PQAp9QaikBUwKg==",
 		"-----END CERTIFICATE-----"
     ],
     :key => [
 		"-----BEGIN PRIVATE KEY-----",
 		"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMUTy7OecnjBFRmM",
-		".........."
+		"..........",
 		"4HRlGtHj8IcOCxNuJMmq6yrTpSyESYAd3wntddLNgie3TFBuPEujZ89kF6oBcn6O",
 		"c7ya5F+abCTDSw==",
 		"-----END PRIVATE KEY-----"
