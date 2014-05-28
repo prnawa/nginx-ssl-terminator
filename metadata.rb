3c738c4b48b0b1b9d668733226744bb1c15f7733
Ruwan Nawarathne <prnawa@gmail.com>
2014-05-28 18:06:46 +0800

    bump the cookbook version

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 description       "nginx as an ssl terminator and reverse proxy"
-version           "0.1.0"
+version           "0.1.1"
 
 %w{ ubuntu debian centos redhat fedora }.each do |os|
   supports os
